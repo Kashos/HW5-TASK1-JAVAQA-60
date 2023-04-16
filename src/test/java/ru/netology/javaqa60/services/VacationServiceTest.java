5f1bbb16c89d3ead53c6338a604d702e7ce304a9
Egor Kachesov <kashos152@gmail.com>
2023-04-16 10:57:17 +0700

    Parameterized Test

--- src/test/java/ru/netology/javaqa60/services/VacationServiceTest.java
+++ src/test/java/ru/netology/javaqa60/services/VacationServiceTest.java
@@ -2,20 +2,24 @@ package ru.netology.javaqa60.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
 
-    @Test
-    public void mustRestForThreeMonths() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/vacation.csv")
+
+    public void mustRestForMonths(int income, int expences, int threshold, int expected) {
 
         VacationService service = new VacationService();
 
-        int expected = 3;
-        int actual = service.calculate(10000, 3000, 20000);
+        int actual = service.calculate(income, expences, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
-
+/*
     @Test
     public void mustRestForTwoMonths() {
 
@@ -26,4 +30,5 @@ public class VacationServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    */
 }
